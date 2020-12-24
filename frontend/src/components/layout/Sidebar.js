4b903e32ad6cae4b45f7bdd1b93b91fe540f5d94
OscarCanongo <oscarcanongo@outlook.com>
2020-12-24 11:43:37 -0600

    Sidebar

--- frontend/src/components/layout/Sidebar.js
+++ frontend/src/components/layout/Sidebar.js
@@ -3,6 +3,37 @@ import logo from '../../images/gpac1.png';
 
 const Sidebar = () => {
 
+    let today = new Date();
+    let day;
+    let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+    let hour = today.getHours() + ':' + today.getMinutes();
+
+    switch (today.getDay()) {
+        case 1:
+            day = "MON"
+            break;
+        case 2:
+            day = "TUES"
+            break;
+        case 3:
+            day = "WED"
+            break;
+        case 4:
+            day = "THURS"
+            break;
+        case 5:
+            day = "FRI"
+            break;
+        case 6:
+            day = "SAT"
+            break;
+        case 7:
+            day = "SUN"
+            break;
+        default:
+            break;
+    }
+    
     return ( 
         <aside className = "aside">
             <img class="centrado" src={logo} />
@@ -44,6 +75,11 @@ const Sidebar = () => {
                     className="btn-aside btn-block" 
                     //onClick={ () => seleccionar(task.id) }
                 > <p> Sendouts</p></button>
+                </ul>
+                <ul className="candidatos">
+                    <p className = "date">{day}</p>
+                    <p className = "date">{date}</p>
+                    <p className = "hour">{hour}</p>
                 </ul>
             </div>
         </aside>
