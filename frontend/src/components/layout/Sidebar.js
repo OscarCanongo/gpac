e3e8ee3e70405efa62edd59ab13ba59bce559474
OscarCanongo <oscarcanongo@outlook.com>
2020-12-23 19:04:59 -0600

    Sidebar

--- frontend/src/components/layout/Sidebar.js
+++ frontend/src/components/layout/Sidebar.js
@@ -1,18 +1,50 @@
 import React from 'react';
-//import NuevoProyecto from '../proyectos/NuevoProyecto';
-//import ListadoProyectos from '../proyectos/ListadoProyectos';
+import logo from '../../images/gpac1.png';
 
 const Sidebar = () => {
+
     return ( 
         <aside className = "aside">
-            <h1 className = "title">LOGO</h1>
+            <img class="centrado" src={logo} />
 
-            {//<NuevoProyecto />
-            }
-            <div className="proyectos">
-                <h2>Listado</h2>
-                {//<ListadoProyectos />
-                }
+            <div className="candidatos">
+                <ul className="listado-candidatos">
+                <button
+                    type="button"
+                    className="btn-aside btn-block" 
+                    //onClick={ () => seleccionar(task.id) }
+                > <p> Dashboard</p></button>
+                <button
+                    type="button"
+                    className="btn-aside btn-block" 
+                    //onClick={ () => seleccionar(task.id) }
+                > <p>Job orders</p></button>
+                <button
+                    type="button"
+                    className="btn-aside btn-block" 
+                    //onClick={ () => seleccionar(task.id) }
+                > <p> Companies</p></button>
+                <button
+                    type="button"
+                    className="btn-aside btn-block" 
+                    //onClick={ () => seleccionar(task.id) }
+                > <p> S.Projects</p></button>
+                <button
+                    type="button"
+                    className="btn-aside btn-block" 
+                    //onClick={ () => seleccionar(task.id) }
+                > <p> Map</p></button>
+                <button
+                    type="button"
+                    className="btn-aside btn-block" 
+                    //onClick={ () => seleccionar(task.id) }
+                > <p> Task Toll</p></button>
+                <button
+                    type="button"
+                    className="btn-aside btn-block" 
+                    //onClick={ () => seleccionar(task.id) }
+                > <p> Sendouts</p></button>
+                </ul>
             </div>
         </aside>
      );
